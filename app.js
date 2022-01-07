efae9574f0101e6131700830ec3ff2471008057c
ywbird <repack.lab.io@gmail.com>
2022-01-07 12:14:26 +0900

    add welcome message

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const Discord = require('discord.js')
 require('dotenv').config()
 
+const generateImage = require("./generateImage")
+
 const client = new Discord.Client({
     intents: [
-        'GUILDS',
-        'GUILD_MESSAGES'
+        "GUILDS",
+        "GUILD_MESSAGES",
+        "GUILD_MEMBERS"
     ]
 })
 
@@ -16,6 +19,16 @@ client.on('messageCreate', (message) => {
     if (message.content == 'hi'){
         message.reply('Hello World!')
     }
+})
+
+const welcomeChannelId = "928826592682197042"
+
+client.on("guildMemberAdd", async (member) => {
+    const img = await generateImage(member)
+    member.guild.channels.cache.get(welcomeChannelId).send({
+        content: `Welcome to **Nycko**'s server, <@${member.id}>!`,
+        files: [img]
+    })
 })
 
 client.login(process.env.TOKEN)
